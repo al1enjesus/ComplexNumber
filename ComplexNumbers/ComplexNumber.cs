1aeac4b085f648378b942bfe2a0b2e4e0d8bcd69
Ilya Gordey <wdybelievein@gmail.com>
2021-03-15 20:23:25 +0300

    add compare operators

--- ComplexNumbers/ComplexNumber.cs
+++ ComplexNumbers/ComplexNumber.cs
@@ -12,24 +12,20 @@ namespace ComplexNumbers {
             _realPart = realPart;
             _imaginePart = imaginePart;
         }
-
-        public float GetRealPart() {
-            return _realPart;
-        }
-
-        public float GetImaginePart() {
-            return _imaginePart;
-        }
+        
+        public float RealPart { get => _realPart; private set { _realPart = value; } }
+        public float ImaginePart { get => _imaginePart; private set { _imaginePart = value; } }
 
         public override int GetHashCode() {
+            RealPart = 0;
             return _realPart.GetHashCode() ^ _imaginePart.GetHashCode();
         }
 
         public static ComplexNumber operator +(ComplexNumber lhs, ComplexNumber rhs) {
             float newReal, newImagine;
             checked {
-                newReal = lhs.GetRealPart() + rhs.GetRealPart();
-                newImagine = lhs.GetImaginePart() + rhs.GetImaginePart();
+                newReal = lhs.RealPart + rhs.RealPart;
+                newImagine = lhs.ImaginePart + rhs.ImaginePart;
             }
             return new ComplexNumber(newReal, newImagine);
         }
@@ -37,8 +33,8 @@ namespace ComplexNumbers {
         public static ComplexNumber operator -(ComplexNumber lhs, ComplexNumber rhs) {
             float newReal, newImagine;
             checked {
-                newReal = lhs.GetRealPart() - rhs.GetRealPart();
-                newImagine = lhs.GetImaginePart() - rhs.GetImaginePart();
+                newReal = lhs.RealPart - rhs.RealPart;
+                newImagine = lhs.ImaginePart - rhs.ImaginePart;
             }
             return new ComplexNumber(newReal, newImagine);
         }
@@ -46,8 +42,8 @@ namespace ComplexNumbers {
         public static ComplexNumber operator *(ComplexNumber lhs, ComplexNumber rhs) {
             float newReal, newImagine;
             checked {
-                newReal = lhs.GetRealPart() * rhs.GetRealPart() - lhs.GetImaginePart() * rhs.GetImaginePart();
-                newImagine = lhs.GetImaginePart() * rhs.GetRealPart() + lhs.GetRealPart() * rhs.GetImaginePart();
+                newReal = lhs.RealPart * rhs.RealPart - lhs.ImaginePart * rhs.ImaginePart;
+                newImagine = lhs.ImaginePart * rhs.RealPart + lhs.RealPart * rhs.ImaginePart;
             }
             return new ComplexNumber(newReal, newImagine);
         }
@@ -55,12 +51,16 @@ namespace ComplexNumbers {
         public static ComplexNumber operator /(ComplexNumber lhs, ComplexNumber rhs) {
             float newReal, newImagine;
             checked {
-                float realNumerator = lhs.GetRealPart() * rhs.GetRealPart() + lhs.GetImaginePart() * rhs.GetImaginePart();
-                float realDenominator = rhs.GetRealPart() * rhs.GetRealPart() + rhs.GetImaginePart() * rhs.GetImaginePart();
-                newReal = realNumerator / realDenominator;
-
-                float imagineNumeartor = lhs.GetImaginePart() * rhs.GetRealPart() - lhs.GetRealPart() * rhs.GetImaginePart();
+                float realNumerator = lhs.RealPart * rhs.RealPart + lhs.ImaginePart * rhs.ImaginePart;
+                float realDenominator = rhs.RealPart * rhs.RealPart + rhs.ImaginePart * rhs.ImaginePart;
+                float imagineNumeartor = lhs.ImaginePart * rhs.RealPart - lhs.RealPart * rhs.ImaginePart;
                 float imagineDenominator = realDenominator;
+
+                if(realDenominator == 0 || imagineDenominator == 0) {
+                    throw new Exception("Division by zero");
+                }
+
+                newReal = realNumerator / realDenominator;
                 newImagine = imagineNumeartor / imagineDenominator;
             }
             return new ComplexNumber(newReal, newImagine);
@@ -70,9 +70,32 @@ namespace ComplexNumbers {
         public static bool operator ==(ComplexNumber lhs, ComplexNumber rhs) {
             return (lhs._realPart == rhs._realPart && lhs._imaginePart == rhs._imaginePart);
         }
-        public static bool operator != (ComplexNumber lhs, ComplexNumber rhs) {
+
+        public static bool operator !=(ComplexNumber lhs, ComplexNumber rhs) {
             return !(lhs == rhs);
         }
+
+        //Сравнения по модулю
+        public static bool operator >(ComplexNumber lhs, ComplexNumber rhs) {
+            float lhsModule, rhsModule;
+            checked {
+                lhsModule = lhs._realPart * lhs._realPart + lhs._imaginePart * lhs._imaginePart;
+                rhsModule = rhs._realPart * rhs._realPart + rhs._imaginePart * rhs._imaginePart;
+            }
+            return lhsModule > rhsModule;
+        }
+        public static bool operator <(ComplexNumber lhs, ComplexNumber rhs) {
+            return (!(lhs > rhs) && !(lhs == rhs));
+        }
+
+        public static bool operator >=(ComplexNumber lhs, ComplexNumber rhs) {
+            return ((lhs > rhs) || (lhs == rhs));
+        }
+
+        public static bool operator <=(ComplexNumber lhs, ComplexNumber rhs) {
+            return ((lhs < rhs) || (lhs == rhs));
+        }
+
         public override string ToString() {
             bool isImaginePartNegative = _imaginePart < 0 ? true : false;
             
--- ComplexNumbers/Program.cs
+++ ComplexNumbers/Program.cs
@@ -4,17 +4,21 @@ using ComplexNumbers;
 namespace ComplexNumbers {
     class Program {
         static void Main(string[] args) {
-            ComplexNumber a = new ComplexNumber(realPart: 2);
-            Console.WriteLine(a);
+            try {
 
-            ComplexNumber b = new ComplexNumber(realPart: -2);
-            Console.WriteLine(b);
-
-            ComplexNumber Num = new ComplexNumber();
-            Console.WriteLine(Num);
-
-            Console.WriteLine(new ComplexNumber(8, 1) / new ComplexNumber(2, -3));
-            Console.WriteLine(new ComplexNumber(7, -3) * new ComplexNumber(7, 3));
+                string answer = "Y";
+                do {
+                    Console.WriteLine("Здравствуйте, вы хотите задать вещественное число? Y|N");
+                    answer = Console.ReadLine();
+                    if (answer == "Y") {
+                        Console.WriteLine("Здравствуйте, вы хотите задать вещественное число? Y|N");
+                    }
+                } while(answer != "N");
+                
+            }
+            catch (Exception e){
+                Console.WriteLine(e);
+            }
         }
     }
 }
