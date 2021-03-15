e05bb93628a84630484b354b0fd90196cd2f54c1
Ilya Gordey <wdybelievein@gmail.com>
2021-03-15 20:58:01 +0300

    add demo main

--- ComplexNumbers/Program.cs
+++ ComplexNumbers/Program.cs
@@ -1,20 +1,110 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using ComplexNumbers;
 
 namespace ComplexNumbers {
     class Program {
         static void Main(string[] args) {
             try {
+                List<ComplexNumber> nums = new List<ComplexNumber>();
 
                 string answer = "Y";
                 do {
-                    Console.WriteLine("Здравствуйте, вы хотите задать вещественное число? Y|N");
+                    Console.WriteLine("Вы хотите задать комплексное число? Y|N");
                     answer = Console.ReadLine();
                     if (answer == "Y") {
-                        Console.WriteLine("Здравствуйте, вы хотите задать вещественное число? Y|N");
+                        float realPart, imaginePart;
+                        Console.WriteLine("Введите действительную часть");
+                        realPart = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+                        Console.WriteLine("Введите мнимую часть");
+                        imaginePart = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+
+                        ComplexNumber temp = new ComplexNumber(realPart, imaginePart);
+                        nums.Add(temp);
+                        Console.WriteLine("Новое число добавлено");
                     }
-                } while(answer != "N");
-                
+                } while(answer == "Y" || answer == "y");
+
+                Console.WriteLine($"Количество заданных чисел: {nums.Count}");
+                if (nums.Count != 0) {
+                    Console.WriteLine("Заданные вами числа");
+                    foreach(var number in nums) {
+                        Console.WriteLine(number);
+                    }
+                }
+
+                answer = "Y";
+                do {
+                    Console.WriteLine("Вы хотите использовать арифметическую операция к вашим числам? Y|N");
+                    answer = Console.ReadLine();
+                    if(answer == "Y") {
+                        string operationType;
+                        Console.WriteLine("Введите операцию '+', '-', '*', '/'");
+                        operationType = Console.ReadLine();
+                        Console.WriteLine($"Введите номер левого числа (индекс[1..{nums.Count}] , среди заданых вами чисел)");
+                        int leftInd = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Введите номер правого числа (индекс[1..{nums.Count}], среди заданых вами чисел)");
+                        int rightInd = int.Parse(Console.ReadLine());
+
+                        Console.Write("Результат применения операции: ");
+                        switch(operationType) {
+                            case "+":
+                                Console.WriteLine(nums[leftInd - 1] + nums[rightInd - 1]);
+                                break;
+                            case "-":
+                                Console.WriteLine(nums[leftInd - 1] - nums[rightInd - 1]);
+                                break;
+                            case "*":
+                                Console.WriteLine(nums[leftInd - 1] * nums[rightInd - 1]);
+                                break;
+                            case "/":
+                                Console.WriteLine(nums[leftInd - 1] / nums[rightInd - 1]);
+                                break;
+
+                        }
+                    }
+                } while(answer == "Y" || answer == "y");
+
+                answer = "Y";
+                do {
+                    Console.WriteLine("Вы хотите использовать операцию сравнения по модулю к вашим числам? Y|N");
+                    answer = Console.ReadLine();
+                    if(answer == "Y") {
+                        string operationType;
+                        Console.WriteLine("Введите операцию '>', '>=', '<', '<=', '==', '!='");
+                        operationType = Console.ReadLine();
+                        Console.WriteLine($"Введите номер левого числа (индекс[1..{nums.Count}] , среди заданых вами чисел)");
+                        int leftInd = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Введите номер правого числа (индекс[1..{nums.Count}], среди заданых вами чисел)");
+                        int rightInd = int.Parse(Console.ReadLine());
+
+                        Console.Write("Результат применения операции: ");
+                        switch(operationType) {
+                            case ">":
+                                Console.WriteLine(nums[leftInd - 1] > nums[rightInd - 1]);
+                                break;
+                            case "<":
+                                Console.WriteLine(nums[leftInd - 1] < nums[rightInd - 1]);
+                                break;
+                            case ">=":
+                                Console.WriteLine(nums[leftInd - 1] >= nums[rightInd - 1]);
+                                break;
+                            case "<=":
+                                Console.WriteLine(nums[leftInd - 1] <= nums[rightInd - 1]);
+                                break;
+                            case "==":
+                                Console.WriteLine(nums[leftInd - 1] == nums[rightInd - 1]);
+                                break;
+                            case "!=":
+                                Console.WriteLine(nums[leftInd - 1] != nums[rightInd - 1]);
+                                break;
+
+                        }
+                    }
+                } while(answer == "Y" || answer == "y");
+
+                Console.WriteLine("Программа завершила работу, спасибо за использование");
             }
             catch (Exception e){
                 Console.WriteLine(e);
